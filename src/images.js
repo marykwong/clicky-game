5275dbb75b000592472ebbd74a6d4e80556935dc
Mary <marykwong27@gmail.com>
2019-07-13 04:01:33 -0700

    update

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import burger1 from './images/burger1.jpg'
+import burger2 from './images/burger2.jpg'
+import burger3 from './images/burger3.jpeg'
+import burger4 from './images/burger4.jpeg'
+import burger5 from './images/burger5.jpg'
+import burger6 from './images/burger6.jpg'
+import burger7 from './images/burger7.jpg'
+import burger8 from './images/burger8.jpg'
+import burger9 from './images/burger9.jpg'
+import burger10 from './images/burger10.jpg'
+import burger11 from './images/burger11.jpg'
+import burger12 from './images/burger12.jpg'
+
+const images = [
+  burger1,
+  burger2,
+  burger3,
+  burger4,
+  burger5,
+  burger6,
+  burger7,
+  burger8,
+  burger9,
+  burger10,
+  burger11,
+  burger12
+]
+
+export default images
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ClickyGame from './App.js'
+import './index.css'
+
+ReactDOM.render(<ClickyGame />, document.getElementById('root'))
